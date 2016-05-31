3497d8c3c30b55974d7d0b94438d764388edfdaa
vkuzbida <vkuzbida@n-ix.com>
2016-06-01 00:30:42 +0300

    proceed

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from 'angular2/core';
+import { Router } from 'angular2/router';
+
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -11,12 +13,19 @@ import { HeroService } from './hero.service';
 export class DashboardComponent implements OnInit {
     heroes: Hero[] = [];
 
-    constructor(private heroService: HeroService) {}
+    constructor(
+        private router: Router,
+        private heroService: HeroService) {
+    }
+
 
     ngOnInit(){
         this.heroService.getHeroes()
             .then(heroes => this.heroes = heroes.slice(1,5))
     }
 
-    gotoDetails(){}
+    gotoDetail(hero: Hero) {
+        let link = ['HeroDetail', { id: hero.id }];
+        this.router.navigate(link);
+    }
 }
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -5,30 +5,23 @@ import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-hero-detail',
-    template: `
-      <button (click)="goBack()">Back</button>
-      <div *ngIf="hero">
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-          <label>name: </label>
-          <input [(ngModel)]="hero.name" placeholder="name"/>
-        </div>
-      </div>
-    `
+    templateUrl: 'app/hero-detail.component.html',
 })
 
 export class HeroDetailComponent implements OnInit {
+    hero: Hero;
+
     constructor(
         private heroService: HeroService,
         private routeParams: RouteParams) {
     }
-    hero: Hero;
+
     ngOnInit() {
         let id = +this.routeParams.get('id');
         this.heroService.getHero(id)
             .then(hero => this.hero = hero);
     }
+
     goBack() {
         window.history.back();
     }
